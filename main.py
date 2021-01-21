6340a06e34c8c7f919525de6f98bda67e5ac2f6c
Rasim <rasimkuchchaev@gmail.com>
2021-01-21 16:44:22 +0300

    options user-agent

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ from selenium import webdriver
 import time
 
 
-url = "https://www.instagram.com"
-driver = webdriver.Chrome(executable_path="C:\\Users\\admin\\Desktop\\PythonTest\\selenium-python\\chromedriver\\chromedriver.exe")
+# url = "https://www.instagram.com"
+# options
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=HelloWorld)")
+
+driver = webdriver.Chrome(
+    executable_path="C:\\Users\\admin\\Desktop\\PythonTest\\selenium-python\\chromedriver\\chromedriver.exe",
+                          options=options
+)
+# driver = webdriver.Chrome(executable_path=r"C:\Users\admin\Desktop\PythonTest\selenium-python\chromedriver\chromedriver.exe")
 
 try:
-    driver.get(url)
+    driver.get(url="http://whatsmyuseragent.org/")
     time.sleep(5)
-    driver.get_screenshot_as_file("1.png")
-    driver.get(url="https://yandex.ru")
-    time.sleep(5)
-    driver.save_screenshot("2.png")
-    driver.refresh()
-    time.sleep(2)
+    # driver.get_screenshot_as_file("1.png")
+    # driver.get(url="https://yandex.ru")
+    # time.sleep(5)
+    # driver.save_screenshot("2.png")
+    # driver.refresh()
+    # time.sleep(2)
 except Exception as ex:
     print(ex)
 finally:
